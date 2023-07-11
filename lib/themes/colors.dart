fadb2be03aef7824aedb2e2019402dedc4dab395
janWICKEDpro <janroyal45@gmail.com>
2023-07-11 19:51:47 +0100

    themes and colors

---
+++ lib/themes/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+final primaryColor = Color.fromARGB(255, 75, 18, 209);
+final darkColor = Color.fromARGB(255, 32, 6, 95);
---
+++ lib/themes/themes.dart
@@ -0,0 +1,5 @@
+class AppThemes {
+  //widget themes
+
+  //text styles
+}
