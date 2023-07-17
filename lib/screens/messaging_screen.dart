ab29311b041d35551d29da9683a2ea6b7443affc
janWICKEDpro <janroyal45@gmail.com>
2023-07-18 00:18:26 +0100

    message screen

--- lib/screens/messaging_screen.dart
+++ lib/screens/messaging_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chat_app/dummy_data/dummy_data.dart';
 import 'package:chat_app/themes/colors.dart';
+import 'package:chat_app/themes/themes.dart';
 import 'package:chat_app/widgets/message_tile.dart';
+import 'package:chat_app/widgets/size_box.dart';
 import 'package:flutter/material.dart';
 
 class MessagingScreen extends StatelessWidget {
@@ -10,7 +12,7 @@ class MessagingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(gradient: gradient),
+        decoration: const BoxDecoration(gradient: gradient),
       ),
       bottomSheet: Container(
         height: MediaQuery.of(context).size.height * 0.91,
@@ -21,6 +23,35 @@ class MessagingScreen extends StatelessWidget {
         ),
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: purple,
+                      ),
+                      sizedBox(width: 5),
+                      Text(
+                        "Xavi Simons",
+                        style: AppThemes().name,
+                      )
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: grey, borderRadius: BorderRadius.circular(30)),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.more_horiz),
+                    ),
+                  )
+                ],
+              ),
+            ),
             ...List.generate(messages.length, (index) {
               if (messages[index].receiverId == '2') {
                 return Align(
